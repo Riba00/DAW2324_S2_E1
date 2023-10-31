a032cd65108d5880698d92cb90b12cea6af2af1b
PaCruDaw <paulacruzado@iesmontsia.org>
2023-10-31 20:08:00 +0100

    connexio API DB

--- fastapi/app/database.py
+++ fastapi/app/database.py
@@ -1,12 +1,39 @@
-# database.py
-
 import mysql.connector
 
-db_config = {
-    "host": "mariadb",
-    "user": "alumne",
-    "password": "alumne1234",
-    "database": "projectx",
-}
+class DatabaseConnection:
+    def __init__(self, host, user, password, database):
+        self.host = host
+        self.user = user
+        self.password = password
+        self.database = database
+        self.connection = None
 
-connection = mysql.connector.connect(**db_config)
+    def connect(self):
+        try:
+            self.connection = mysql.connector.connect(
+                host=self.host,
+                user=self.user,
+                password=self.password,
+                database=self.database
+            )
+            print("La connexió ha tingut exit")
+        except mysql.connector.Error as err:
+            print(f"Error al connectar: {err}")
+
+    def disconnect(self):
+        if self.connection and self.connection.is_connected():
+            self.connection.close()
+            print("Connexió tancada.")
+
+    def execute_query(self, query, values=None):
+        if not self.connection or not self.connection.is_connected():
+            print("No hi ha connexió.")
+            return
+
+        cursor = self.connection.cursor(dictionary=True)
+        cursor.execute(query, values)
+        result = cursor.fetchall()
+        cursor.close()
+        return result
+
+
