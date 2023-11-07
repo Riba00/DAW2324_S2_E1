e89a946bc9b92679d2a4827214ee0d1a9b049a23
PaCruDaw <paulacruzado@iesmontsia.org>
2023-11-07 15:45:18 +0100

    para cierre merge

--- Aplicatiuweb/bootstrap/js/product_detail.js
+++ Aplicatiuweb/bootstrap/js/product_detail.js
@@ -2,8 +2,8 @@ function changeImage(element) {
       var imatge_producte = document.getElementById('imatge_producte');
       imatge_producte.src = element.src;      
 }
-/*  
+ 
 function sumarUnProducte() {
       document.getElementById("cantitat_productes").value ="Hola"
      
-}  */
+}  
