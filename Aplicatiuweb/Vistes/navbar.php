6e165ec4da0d063f0d6b1cd22b765fc9cf2b8d05
Riba00 <jordi.riba.ferre@gmail.com>
2023-11-07 16:04:13 +0100

    link riba with paula

--- Aplicatiuweb/Vistes/navbar.php
+++ Aplicatiuweb/Vistes/navbar.php
@@ -21,7 +21,7 @@
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ml-auto">
                     <li class="nav-item">
-                        <a class="nav-link text-white" href="#">Productos</a>
+                        <a class="nav-link text-white" href="/Vistes/productes.php">Productos</a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link text-white" href="/Vistes/imagePrompt.php">Generar Imagen</a>
