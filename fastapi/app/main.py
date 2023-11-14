fd1ac7c8b974f6526f6712b3e8f8c17282116fdc
JordiNavGom <jordinavarro@iesmontsia.org>
2023-11-14 21:04:30 +0100

    Api finalitzada amb comentaris

--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,3 +1,4 @@
+# Importar los módulos necesarios
 from typing import Union
 import base64
 import requests
@@ -6,30 +7,41 @@ from jose import JWTError, jwt
 from fastapi.security import OAuth2PasswordBearer 
 from dotenv import load_dotenv
 import os
+from fastapi import Form
 
+# Cargar las variables de entorno desde el archivo .env
 load_dotenv()
 
+# Configurar la URL de la API externa
 url = "https://api.picanova.com/api/beta"
+
+# Inicializar la aplicación FastAPI
 app = FastAPI()
 
-# Credenciales para la API externa
+# Obtener las variables de entorno
+SECRET_KEY = os.getenv("SECRET_KEY")
+ALGORITHM = os.getenv("ALGORITHM")
+
+# Configurar el esquema de autenticación OAuth2 con contraseña
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+# Definir las credenciales de la API externa
 external_api_user = "virtual-vision"
 external_api_password = "2b8af5289aa93fc62eae989b4dcc9725"
 
+# Codificar las credenciales en Base64 para el encabezado de autorización
 encriptacio =  base64.b64encode(f"{external_api_user}:{external_api_password}".encode("utf-8")).decode("utf-8")
 headerspica = {
     "Authorization": f"Basic {encriptacio}"
 }
-# Variables de entorno
-SECRET_KEY = os.getenv("SECRET_KEY")
-ALGORITHM = os.getenv("ALGORITHM")
-
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # Funciones auxiliares
+
+# Función para generar un token JWT
 def create_token(data: dict):
     return jwt.encode(data, SECRET_KEY, algorithm=ALGORITHM)
 
+# Función para obtener el usuario actual a partir del token JWT
 def get_current_user(token: str = Depends(oauth2_scheme)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
@@ -37,42 +49,54 @@ def get_current_user(token: str = Depends(oauth2_scheme)):
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
+        # Decodificar el token y obtener la información del usuario
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         return payload
     except JWTError:
+        # Capturar errores relacionados con el token
         raise credentials_exception
+    
+# Función para verificar las credenciales de usuario
+def verify_credentials(username: str, password: str):
+    return username == external_api_user and password == external_api_password
 
-# Rutas protegidas
+# Definir la ruta para autenticar y obtener un token
 @app.post("/token")
-def login():
+def login(username: str = Form(...), password: str = Form(...)):
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Invalid credentials",
         headers={"WWW-Authenticate": "Bearer"},
     )
     try:
-        # Realiza la autenticación y verifica las credenciales
-        # Puedes adaptar esta lógica según tus necesidades específicas
-        if True:  # Reemplaza esto con tu lógica de autenticación
-            # Ejemplo de datos que se pueden incluir en el token
-            data = {"sub": external_api_user}
+        # Verificar las credenciales proporcionadas
+        if verify_credentials(username, password):
+            # Credenciales válidas, generar un token
+            data = {"sub": username}
             access_token = create_token(data)
             return {"access_token": access_token, "token_type": "bearer"}
         else:
+            # Credenciales inválidas, lanzar una excepción
             raise credentials_exception
     except Exception as e:
+        # Capturar otros errores
         return {"message": f"Error: {str(e)}"}
 
+# Definir la ruta para obtener productos (requiere autenticación)
 @app.get("/products")
 def get_products(current_user: dict = Depends(get_current_user)):
     try:
+        # Realizar una solicitud a la API externa con el encabezado de autorización
         response = requests.get(url + "/products", headers=headerspica)
         
         if response.status_code == 200:
+            # Si la solicitud es exitosa, devolver los productos
             products = response.json()
             return {"data": products}
         else:
+            # Si la solicitud no es exitosa, devolver un mensaje de error
             return {"message": f"Error al obtener productos. Código de estado: {response.status_code}"}
 
     except Exception as e:
+        # Capturar otros errores
         return {"message": f"Error: {str(e)}"}
