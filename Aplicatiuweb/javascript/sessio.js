e9a542dfd1f6fb7df151e04284ca1f7667484acb
Jordinavgom <117455643+Jordinavgom@users.noreply.github.com>
2023-11-15 11:37:45 +0100

    Documentar sessió

--- Aplicatiuweb/javascript/sessio.js
+++ Aplicatiuweb/javascript/sessio.js
@@ -1,7 +1,7 @@
-$(document).ready(function() {
-    if (typeof sesionIniciada !== 'undefined' && sesionIniciada) {
-        console.log("Usuario ha iniciado sesión previamente.");
-        var html = '<li class="nav-item position-absolute end-0 me-5" id="registre-li">' +
+$(document).ready(function() { //no es fa la funció fins que no carrega el document
+    if (typeof sesionIniciada !== 'undefined' && sesionIniciada) { // Comprovem si la variable 'sesionIniciada' està definida i és certa.
+        console.log("Usuario ha iniciado sesión previamente."); // Mostrar per consola si l'usuari ha inciat sessió o no
+        var html = '<li class="nav-item position-absolute end-0 me-5" id="registre-li">' + // Creem un fragment HTML amb un enllaç al perfil de l'usuari.
             '<a class="nav-link text-white" href="/Vistes/perfil.php">Perfil</a>' +
             '</li>';
         $('#registre-li').html(html); //Amb jquery afegirem el contingut a l'etiqueta
